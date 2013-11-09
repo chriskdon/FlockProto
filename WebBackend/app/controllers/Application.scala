841cbb8188545d41465577d0363e4455175f589c
Chris <chriskellendonk@gmail.com>
2013-11-09 15:59:23 -0500

    Basic connections

--- WebBackend/app/controllers/Application.scala
+++ WebBackend/app/controllers/Application.scala
@@ -1,12 +1,19 @@
 package controllers
 
+import play.api.Play.current
 import play.api._
 import play.api.mvc._
+import play.api.db._
+import anorm._
 
 object Application extends Controller {
   
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    DB.withConnection { implicit conn =>
+      val row = SQL("SELECT COUNT(*) AS c FROM Users").apply().head
+
+      Ok(row[Long]("c").toString)
+    }
   }
   
 }
