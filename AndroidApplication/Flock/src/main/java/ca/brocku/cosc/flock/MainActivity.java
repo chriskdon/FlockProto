d33ef34c820f9883705880dbaedaa47977a0d1ac
kubasub <kubasub12@gmail.com>
2013-11-13 19:05:58 -0500

    Added bare UI for Login and Registration

--- AndroidApplication/Flock/src/main/java/ca/brocku/cosc/flock/MainActivity.java
+++ AndroidApplication/Flock/src/main/java/ca/brocku/cosc/flock/MainActivity.java
@@ -42,6 +42,7 @@ public class MainActivity extends Activity implements View.OnClickListener{
         if (id == R.id.action_settings) {
             return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 
