0e150be2547433cbb6415bdd269d737f52c2effa
kubasub <kubasub12@gmail.com>
2013-11-17 13:31:49 -0500

    properly merged registration section of app

--- AndroidApplication/Flock/src/main/java/ca/brocku/cosc/flock/RegisterActivity.java
+++ AndroidApplication/Flock/src/main/java/ca/brocku/cosc/flock/RegisterActivity.java
@@ -1,39 +1,23 @@
 package ca.brocku.cosc.flock;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 import android.widget.Button;
 import android.widget.EditText;
-<<<<<<< HEAD
 import android.widget.FrameLayout;
 import android.widget.TextView;
-
-public class RegisterActivity extends Activity implements View.OnClickListener {
-    FrameLayout loginWrapper;
-    Button registerBtn;
-    EditText firstname, lastname, username, password;
-    TextView error;
-=======
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import ca.brocku.cosc.flock.data.ApplicationSettings;
 import ca.brocku.cosc.flock.data.api.json.models.GenericSuccessModel;
 
-public class RegisterActivity extends Activity {
-    private Button submitButton; // Button to register a new user
+public class RegisterActivity extends Activity implements View.OnClickListener {
+    FrameLayout loginWrapper;
+    private Button registerButton; // Button to register a new user
     private EditText firstNameInput, lastNameInput, usernameInput, passwordInput;
->>>>>>> da57a8cecc6b2d965e8f236ecebddbd651ebe863
+    TextView error;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,52 +25,40 @@ public class RegisterActivity extends Activity {
         setContentView(R.layout.activity_register);
         getActionBar().hide();
 
-<<<<<<< HEAD
-=======
         // Initialze Application Settings
         ApplicationSettings.initialize(getBaseContext());
 
         // Bind Controls
-        submitButton = (Button)findViewById(R.id.submit_registration_button);
+        loginWrapper = (FrameLayout)findViewById(R.id.login_expand_wrapper);
+        registerButton = (Button)findViewById(R.id.submit_registration_button);
         firstNameInput = (EditText)findViewById(R.id.firstName_input);
         lastNameInput = (EditText)findViewById(R.id.lastName_input);
         usernameInput = (EditText)findViewById(R.id.username_input);
         passwordInput = (EditText)findViewById(R.id.password_input);
-
-        // Bind Handlers
-        submitButton.setOnClickListener(new RegisterSubmitHandler());
-    }
->>>>>>> da57a8cecc6b2d965e8f236ecebddbd651ebe863
-
-        loginWrapper = (FrameLayout)findViewById(R.id.login_expand_wrapper);
-        registerBtn = (Button) findViewById(R.id.login_btn);
-        firstname = (EditText) findViewById(R.id.firstName_input);
-        lastname = (EditText) findViewById(R.id.lastName_input);
-        username = (EditText) findViewById(R.id.username_input);
-        password = (EditText) findViewById(R.id.password_input);
         error = (TextView) findViewById(R.id.login_errorMsg);
 
+        // Bind Handlers
+        registerButton.setOnClickListener(new RegisterSubmitHandler());
         loginWrapper.setOnClickListener(this);
-        registerBtn.setOnClickListener(this);
     }
 
     @Override
-<<<<<<< HEAD
     public void onClick(View v) {
         int id = v.getId();
 
         if(loginWrapper.getId() == v.getId()) {
             finish();
             startActivity(new Intent(this, LoginActivity.class));
-        }else if (id == registerBtn.getId()) { //clicked to submit login credentials
+        }else if (id == registerButton.getId()) { //clicked to submit login credentials
             register();
         }
     }
 
     private void register() {
-        if (!firstname.getText().toString().isEmpty() && !lastname.getText().toString().isEmpty() && !username.getText().toString().isEmpty() && !password.getText().toString().isEmpty()) { //if all fields aren't null, try to login
+        if (!firstNameInput.getText().toString().isEmpty() && !lastNameInput.getText().toString().isEmpty() && !usernameInput.getText().toString().isEmpty() && !passwordInput.getText().toString().isEmpty()) { //if all fields aren't null, try to login
             try {
-                //String secret = API.register(firstname.getText().toString(), lastname.getText().toString(), username.getText().toString(), password.getText().toString()); //TODO: call appropriate method to authenticate user and get secret
+                //TODO: call appropriate method to register user and get secret
+                //String secret = API.register(firstNameInput.getText().toString(), lastNameInput.getText().toString(), usernameInput.getText().toString(), passwordInput.getText().toString());
                 SharedPreferences prefs = getSharedPreferences(getString(R.string.sharedPrefsKey), MODE_PRIVATE);
                 SharedPreferences.Editor prefsEditor = prefs.edit();
                 //prefsEditor.putString("SECRET", secret);
@@ -99,28 +71,18 @@ public class RegisterActivity extends Activity {
         } else { //inform the user that all fields must be filled in
             error.setText("Please fill in all fields.");
             error.setVisibility(View.VISIBLE);
-=======
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
         }
-        return super.onOptionsItemSelected(item);
     }
 
     /**
      * Handles the user clicking the submit button when trying to register
      * as a new user.
      */
-    private class RegisterSubmitHandler implements Button.OnClickListener {
+    class RegisterSubmitHandler implements Button.OnClickListener {
         @Override
         public void onClick(View v) {
             // TODO: Register a new user
             firstNameInput.setText((new GenericSuccessModel("Test")).toJsonString());
->>>>>>> da57a8cecc6b2d965e8f236ecebddbd651ebe863
         }
     }
 }
