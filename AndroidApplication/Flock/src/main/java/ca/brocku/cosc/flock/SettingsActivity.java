4f24448c4f6cfbc447bb7b6c6fcc0c0f5b26a1fd
kubasub <kubasub12@gmail.com>
2013-12-05 17:06:49 -0500

    Added confirmation dialog when trying to delete account.

--- AndroidApplication/Flock/src/main/java/ca/brocku/cosc/flock/SettingsActivity.java
+++ AndroidApplication/Flock/src/main/java/ca/brocku/cosc/flock/SettingsActivity.java
@@ -1,13 +1,21 @@
 package ca.brocku.cosc.flock;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import ca.brocku.cosc.flock.data.api.FlockAPIResponseHandler;
+import ca.brocku.cosc.flock.data.api.actions.FlockUserAPIAction;
+import ca.brocku.cosc.flock.data.api.json.models.GenericErrorModel;
+import ca.brocku.cosc.flock.data.api.json.models.user.LoginUserResponseModel;
 import ca.brocku.cosc.flock.data.settings.UserDataManager;
 
 /**
@@ -16,6 +24,7 @@ import ca.brocku.cosc.flock.data.settings.UserDataManager;
 public class SettingsActivity extends Activity {
     private Switch locationSwitch;
     private Button logoutButton, deleteAccountButton;
+    private Dialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,13 +54,55 @@ public class SettingsActivity extends Activity {
         @Override
         public void onClick(View v) {
             new UserDataManager(getBaseContext()).removeUserSecret();
+            finish();
             startActivity(new Intent(getBaseContext(), MainActivity.class));
         }
     }
 
     private class DeleteAccountHandler implements View.OnClickListener {
+        private Button cancelButton, acceptButton;
+        private EditText passwordInput;
+        private TextView errorMessage;
+
         @Override
         public void onClick(View v) {
+            dialog = new Dialog(SettingsActivity.this);
+            dialog.setContentView(R.layout.stub_delete_confirmation);
+            dialog.setTitle("Are you sure?");
+            dialog.show();
+
+            cancelButton = (Button) dialog.findViewById(R.id.cancel_delete_notification_button);
+            acceptButton = (Button) dialog.findViewById(R.id.accept_delete_notification_button);
+            passwordInput = (EditText) dialog.findViewById(R.id.delete_confirmation_password_input);
+            errorMessage = (TextView) dialog.findViewById(R.id.delete_confirmation_error_message);
+
+
+            cancelButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    passwordInput.setText("");
+                    dialog.hide();
+                }
+            });
+
+            acceptButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String password = passwordInput.getText().toString();
+
+                    // Make sure fields aren't empty
+                    if (!password.isEmpty()) {
+                        //TODO: delete the user
+
+                        new UserDataManager(getBaseContext()).removeUserSecret();
+                        finish();
+                        startActivity(new Intent(SettingsActivity.this, RegisterActivity.class));
+                    } else { //inform the user that all fields must be filled in
+                        errorMessage.setText("Invalid password.");
+                        errorMessage.setVisibility(View.VISIBLE);
+                    }
+                }
+            });
 
         }
     }
